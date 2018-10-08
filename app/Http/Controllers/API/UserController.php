ecefdbba230164d858eb20736ed3ecf697b69269
rawpioloco <andunker@hotmail.com>
2018-10-07 21:59:57 -0500

    logout token

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,9 +3,15 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\User;
+
 use Illuminate\Support\Facades\Auth;
 use Validator;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use App\Direccione;
+use App\Cliente;
+use App\Proveedore;
+use Lcobucci\JWT\Parser;
 
 class UserController extends Controller
 {
@@ -32,22 +38,57 @@ class UserController extends Controller
      */
     public function register(Request $request)
     {
+
         $validator = Validator::make($request->all(), [
             'name' => 'required',
+            'id_tipo_documento' => 'required',
+            'documento' => 'required',
             'username' => 'required',
             'email' => 'required|email',
             'password' => 'required',
             'c_password' => 'required|same:password',
+            'direccion' => 'required',
+            'id_ciudad' => 'required'
         ]);
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 401);
         }
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] = $user->createToken('MyApp')->accessToken;
-        $success['name'] = $user->name;
-        return response()->json(['success' => $success], $this->successStatus);
+
+        $data = $request->all();
+
+        $usuario = User::create([
+            'name' => $data['name'],
+            'id_tipo_documento' => $data['id_tipo_documento'],
+            'documento' => $data['documento'],
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        Direccione::create([
+            'direccion_txt' => $data['direccion'],
+            'ciudad_id' => $data['id_ciudad'],
+            'user_id' => $usuario->id
+        ]);
+
+        Cliente::create([
+            'user_id' => $usuario->id
+        ]);
+
+        $data['proveedor'] = 'on';
+
+        Proveedore::create([
+            'user_id' => $usuario->id
+        ]);
+
+        $success['token'] = $usuario->createToken('MyApp')->accessToken;
+        $success['name'] = $usuario->name;
+
+        return response()->json([
+            'usuario' => $usuario,
+            'success' => $success
+        ], $this->successStatus);
+
     }
     /** 
      * details api 
@@ -59,4 +100,15 @@ class UserController extends Controller
         $user = Auth::user();
         return response()->json(['success' => $user], $this->successStatus);
     }
+
+    public function logout(Request $request)
+    {
+        $value = $request->bearerToken();
+        $id = (new Parser())->parse($value)->getHeader('jti');
+        $token = $request->user()->tokens->find($id);
+        $token->revoke();
+        $response = 'You have been successfully logged out!';
+        return response($response, 200);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Http\Request;
 */
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
+
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('details', 'API\UserController@details');
+    Route::get('logout', 'API\UserController@logout');
+   
 });
