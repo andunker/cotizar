fe8b0845f34d8a15cd673d5d981b3c23aab8cdcb
rawpioloco <andunker@hotmail.com>
2018-10-10 14:13:53 -0500

    Servicio responder cotizacion

--- app/Http/Controllers/API/CotizacioneController.php
+++ app/Http/Controllers/API/CotizacioneController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 use Lcobucci\JWT\Parser;
 use App\Cotizacione;
 use DateTime;
+use App\RespuestasCotizacione;
 
 
 class CotizacioneController extends Controller
@@ -57,4 +58,55 @@ class CotizacioneController extends Controller
         ], 
         $this->successStatus);
     }
+
+    /** 
+     * logout api 
+     * 
+     * @return \Illuminate\Http\Response 
+     */
+    public function responder_cotizacion(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'cotizacion_id' => 'required',
+            'proveedor_id' => 'required',
+            'dias_envio_promedio' => 'required',
+            'detalle' => 'required',
+            'json_productos' => 'required',
+            'fecha_cierre' => 'required',
+            'tipo_envio_id' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        $validator = Validator::make(json_decode($request->all()['json_productos']), [
+            'id' => 'required',
+            'cantidad' => 'required',
+            'categoria' => 'required'
+        ]);
+
+        $data = $request->all();
+
+        $RespuestasCotizacion = RespuestasCotizacione::create([
+
+            'cotizacion_id' => $data['cotizacion_id'],
+            'proveedor_id' => $data['proveedor_id'],
+            'dias_envio_promedio' => $data['dias_envio_promedio'],
+            'detalle' => $data['detalle'],
+            'json_productos' => $data['json_productos'],
+            //'fecha_cierre' => $data['fecha_cierre'],
+            'fecha_cierre' => new DateTime($data['fecha_cierre']),
+            'tipo_envio_id' => $data['tipo_envio_id']
+        ]);
+
+        return response()->json(
+            [
+                'success' => 'success',
+                'cotizacion_id' => $RespuestasCotizacion['id']
+            ],
+            $this->successStatus
+        );
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,11 @@ use Illuminate\Http\Request;
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
+
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('details', 'API\UserController@details');
     Route::get('logout', 'API\UserController@logout');
     Route::post('cotizar', 'API\CotizacioneController@cotizar');
+    Route::post('responder_cotizacion', 'API\CotizacioneController@responder_cotizacion');
    
 });
